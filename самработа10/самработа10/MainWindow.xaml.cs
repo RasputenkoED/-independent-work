524368c86f8ac88010d4cf9762a6102d0c3de5be
RasputenkoED <106722996+RasputenkoED@users.noreply.github.com>
2022-10-27 16:58:32 +0500

    Add files via upload

---
+++ самработа10/самработа10/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace самработа10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>  
+    public partial class MainWindow : Window
+    {
+        private Random _random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Random_Click(object sender, RoutedEventArgs e)
+        {
+            Data data = new Data() { RandomValue = _random.Next(100000) };
+            DataContext = data;
+        }
+
+        class Data
+        {
+            public int RandomValue { get; set; }
+        }
+    }
+}
